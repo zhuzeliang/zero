727a131765132721ffa09c8542a365f2a7e349e4
zzl <389409258@qq.com>
2017-07-12 16:21:53 +0800

    add

---
+++ js/rem.js
@@ -0,0 +1,23 @@
+(function (doc, win) {
+	    var docEl = doc.documentElement,
+	        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+	        recalc = function () {
+	            var clientWidth = docEl.clientWidth;
+	            if (!clientWidth) return;
+	            if(clientWidth>=750){
+	                docEl.style.fontSize = '100px'; 
+	            }else{
+	                docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+	            }
+	        };
+	    if (!doc.addEventListener) return;
+	    win.addEventListener(resizeEvt, recalc, false);
+	    doc.addEventListener('DOMContentLoaded', recalc, false);
+	})(document, window);
+
+	// 页面加载中方法
+	function loadFun(){
+		setTimeout(function(){
+			$(".my_loading").hide();
+		},500)
+	};
